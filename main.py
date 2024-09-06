51df6ab273bddf57823a6e286c9e4595cbbb1be1
abdullahjawaid30 <abdullahjawaid30@gmail.com>
2024-09-06 17:02:32 +0500

    Adding Dynamically blogs in website

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from flask_mail import Mail
 import json
 
+with open("config.json", 'r') as c:
+    params = json.load(c)["params"]
+    print(params)  # This will print in your Flask console
 
-with open("config.json",'r') as c:
-    params=json.load(c)["params"]
-    
 local_server=True 
     
 app = Flask(__name__)
@@ -27,15 +27,37 @@ class Contacts(db.Model):
     msg = db.Column(db.String(120), nullable=True)
     date = db.Column(db.String(12), nullable=False)
     
+
+class Posts(db.Model):
+    __tablename__ = 'post'  # Use the actual table name in the database
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(25), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
+    img_file = db.Column(db.String(12), nullable=False)
     
+   
 
 @app.route("/")
 def home():
-    return render_template('index.html',params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_Posts']]
+    return render_template('index.html', params=params, posts=posts)
+
+
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('login.html', params=params)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html',params=params)
+
+
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
@@ -49,8 +71,18 @@ def contact():
         db.session.commit()
     return render_template('contact.html',params=params)
 
-@app.route("/post/<string:post_slug>",methods=['GET'])
-def post():
-    return render_template('post.html',params=params)
+
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    if post is None:
+        return "Post not found", 404
+    return render_template('post.html', params=params, post=post)
+
+
+
+
 
 app.run(debug=True)
